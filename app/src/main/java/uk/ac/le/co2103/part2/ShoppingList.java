f1ad9ff88a85c64c598e1bc3f3136fcc7b130561
davidlawal17 <d.lawal@hotmail.com>
2024-04-30 00:34:00 +0100

    Creating ShoppingList class and ShoppingList DAO

---
+++ app/src/main/java/uk/ac/le/co2103/part2/ShoppingList.java
@@ -0,0 +1,45 @@
+package uk.ac.le.co2103.part2;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.util.List;
+
+@Entity(tableName = "ShoppingList_table")
+
+public class ShoppingList {
+
+    @PrimaryKey
+    private int listId;
+
+    private String name;
+
+    private String image;
+
+   // private List<Product> products;
+
+
+    public int getListId() {
+        return listId;
+    }
+
+    public void setListId(int listId) {
+        this.listId = listId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
---
+++ app/src/main/java/uk/ac/le/co2103/part2/ShoppingListDao.java
@@ -0,0 +1,25 @@
+package uk.ac.le.co2103.part2;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface ShoppingListDao {
+
+        @Insert(onConflict = OnConflictStrategy.IGNORE)
+        void insert(ShoppingList shoppinglist);
+
+        @Query("DELETE FROM ShoppingList_table")
+        void deleteAll();
+
+        @Query("SELECT * From ShoppingList_table ORDER BY name ASC")
+        List<ShoppingList> getAlphabetisedShoppingLists();
+
+
+
+
+}
