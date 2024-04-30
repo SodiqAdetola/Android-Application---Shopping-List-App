53e3969cc10925035011f03885731ace463d07c5
sodiq <sa992@student.le.ac.uk>
2024-04-30 18:44:07 +0100

    files packages removed.

--- app/src/main/java/uk/ac/le/co2103/part2/ProductDB.java
+++ app/src/main/java/uk/ac/le/co2103/part2/ProductDB.java
@@ -1,4 +1,4 @@
-package uk.ac.le.co2103.part2.database;
+package uk.ac.le.co2103.part2;
 
 import android.content.Context;
 
@@ -9,25 +9,22 @@ import androidx.room.RoomDatabase;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import uk.ac.le.co2103.part2.Dao.ProductDao;
-import uk.ac.le.co2103.part2.domain.Product;
-
 @Database(entities = {Product.class}, version = 1, exportSchema = false)
-abstract class ProductRoomDatabase extends RoomDatabase {
+public abstract class ProductDB extends RoomDatabase {
 
     public abstract ProductDao productDao();
 
-    private static volatile ProductRoomDatabase INSTANCE;
+    private static volatile ProductDB INSTANCE;
     private static final int NUMBER_OF_THREADS = 4;
     static final ExecutorService databaseWriteExecutor =
             Executors.newFixedThreadPool(NUMBER_OF_THREADS);
 
-    static ProductRoomDatabase getDatabase(final Context context) {
+    static ProductDB getDatabase(final Context context) {
         if (INSTANCE == null) {
-            synchronized (ProductRoomDatabase.class) {
+            synchronized (ProductDB.class) {
                 if (INSTANCE == null) {
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
-                                    ProductRoomDatabase.class, "word_database")
+                                    ProductDB.class, "word_database")
                             .build();
                 }
             }
---
+++ app/src/main/java/uk/ac/le/co2103/part2/ProductRepository.java
@@ -0,0 +1,25 @@
+package uk.ac.le.co2103.part2;
+
+import android.app.Application;
+
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class ProductRepository {
+    private ProductDao productDao;
+    private LiveData<List<Product>> allProducts;
+    ProductRepository(Application application) {
+        ProductDB db = ProductDB.getDatabase(application);
+        productDao = db.productDao();
+        allProducts = productDao.getAlphabetisedProducts();
+    }
+    LiveData<List<Product>> getAllProducts() {
+        return allProducts;
+    }
+    void insert(Product product) {
+        ProductDB.databaseWriteExecutor.execute(() -> {
+            productDao.insert(product);
+        });
+    }
+}
