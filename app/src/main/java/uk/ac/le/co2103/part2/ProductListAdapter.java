f27120ad4f972a93bd54d5ebb34880a4fe16da33
sodiq <sa992@student.le.ac.uk>
2024-05-02 01:51:23 +0100

    ProductListAdapter created

--- app/src/main/java/uk/ac/le/co2103/part2/ProductListAdapter.java
+++ app/src/main/java/uk/ac/le/co2103/part2/ProductListAdapter.java
@@ -1,4 +1,35 @@
 package uk.ac.le.co2103.part2;
 
-public class ProductListAdapter {
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.DiffUtil;
+import androidx.recyclerview.widget.ListAdapter;
+
+public class ProductListAdapter  extends ListAdapter<Product, ProductViewHolder>{
+    public ProductListAdapter(@NonNull DiffUtil.ItemCallback<Product> diffCallback)
+    {
+        super(diffCallback);
+    }
+    @Override
+    public ProductViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
+    {
+        return ProductViewHolder.create(parent);
+    }
+    @Override
+    public void onBindViewHolder(ProductViewHolder holder, int position) {
+        Product current = getItem(position);
+        holder.bind(current.getProduct());
+    }
+    static class ProductDiff extends DiffUtil.ItemCallback<Product> {
+        @Override
+        public boolean areItemsTheSame(@NonNull Product oldProduct, @NonNull Product newProduct) {
+            return oldProduct == newProduct;
+        }
+        @Override
+        public boolean areContentsTheSame(@NonNull Product oldProduct, @NonNull Product newProduct) {
+            return oldProduct.getName().equals(newProduct.getName());
+        }
+    }
 }
